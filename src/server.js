ca5ac867bfaab460fe7fc27a72eb423fa6af8374
Thomas Hoddinott <thomashoddinott477@gmail.com>
2022-02-18 21:48:09 +0100

    fin

--- src/server.js
+++ src/server.js
@@ -12,4 +12,40 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+// app.use(log) // run for everything
+
+const router = express.Router()
+
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.use('/api', router)
+
+//shorthand
+router.route('/cat')
+  .get()
+  .post()
+router.route('/cat/:id')
+  .get()
+  .put()
+  .delete()
+
+// can also pass an array of middlewares
+app.get('/data', log, (req, res) => {
+  res.send({ message: 'hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
